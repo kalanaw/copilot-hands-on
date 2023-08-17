282cdfd80b364fbc9b4eda88cd5975d0e0398005
harshanae99x <136785938+harshanae99x@users.noreply.github.com>
2023-08-17 20:06:15 +0530

    added meeting room filteration logic on FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from './components/login/login';
 import {Routes, Route} from 'react-router-dom';
+import { HomePage } from './components/home-page/home-page';
 
 function App() {
   return (
@@ -10,14 +11,7 @@ function App() {
       <h1>
       Welcome to copilot hands on session
       </h1>
-      {/* configure routes, home and login */}
-      {/* <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<G />} />
-      </Routes> */}
-
-
+      <Login></Login>
     </div>
   );
 }
--- frontend/src/components/home-page/home-page.tsx
+++ frontend/src/components/home-page/home-page.tsx
@@ -3,24 +3,34 @@ import React, {useState} from "react";
 
 interface HomePageProps {}
 
+interface MeetingRoom {
+  meetingRoomId: number;
+  name: string;
+  capacity: number;
+  bookedTimeSlots: {
+    startTime: string;
+    endTime: string;
+  }[];
+}
+
 export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
   // home page displays a list of meeting rooms that are available for booking
   // each meeting room has a name, capacity and and booked time slots
 
   // initize the state for meeting rooms
-  const [meetingRooms, setMeetingRooms] = useState([
+  const [meetingRooms, setMeetingRooms] = useState<MeetingRoom[]>([
     {
       meetingRoomId: 1,
       name: "Meeting Room 1",
       capacity: 10,
       bookedTimeSlots: [
         {
-          startTime: "2021-07-01T09:00:00",
-          endTime: "2021-07-01T10:00:00",
+          startTime: "2023-08-18T09:00:00",
+          endTime: "2023-08-18T10:00:00",
         },
         {
-          startTime: "2021-07-01T11:00:00",
-          endTime: "2021-07-01T12:00:00",
+          startTime: "2023-08-18T12:00:00",
+          endTime: "2023-08-18T13:00:00",
         },
       ],
     },
@@ -30,18 +40,32 @@ export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
       capacity: 5,
       bookedTimeSlots: [
         {
-          startTime: "2021-07-01T09:00:00",
-          endTime: "2021-07-01T10:00:00",
+          startTime: "2023-08-18T08:00:00",
+          endTime: "2023-08-18T09:00:00",
         },
         {
-          startTime: "2021-07-01T11:00:00",
-          endTime: "2021-07-01T12:00:00",
+          startTime: "2023-08-18T14:00:00",
+          endTime: "2023-08-18T15:00:00",
         },
       ],
     },
-  ])
+  ]),
+  [availableMeetingRooms, setAvailableMeetingRooms] = useState<MeetingRoom[]>([]);
+  //initialize formstate for start time and end time
+  const [formState, setFormState] = useState({
+    startTime: {
+      value: "",
+      isValid: false,
+      errorMessage: "",
+    },
+    endTime: {
+      value: "",
+      isValid: false,
+      errorMessage: "",
+    },
+  }),
   // write a function to find the available meeting rooms given a start time and end time
-  const findAvailableMeetingRooms = (startTime: string, endTime: string) => {
+  findAvailableMeetingRooms = (startTime: string, endTime: string) => {
     // loop through the meeting rooms and find the ones that have no time conflicts
     const availableMeetingRooms = meetingRooms.filter((meetingRoom: any) => {
       // check if the current meeting room has a time conflict with the start and end time
@@ -49,13 +73,49 @@ export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
         // check if the start time and end time fall in between the booked time slots
         const startTimeConflict = new Date(bookedTimeSlot.startTime) <= new Date(startTime) && new Date(bookedTimeSlot.endTime) >= new Date(startTime),
         endTimeConflict = new Date(bookedTimeSlot.startTime) <= new Date(endTime) && new Date(bookedTimeSlot.endTime) >= new Date(endTime);
+        console.log(new Date(bookedTimeSlot.startTime), new Date(startTime), new Date(bookedTimeSlot.endTime), new Date(endTime));
         return startTimeConflict || endTimeConflict;
       });
       // if there is no time conflict, return the meeting room
       return !hasTimeConflict;
     });
+    console.log(availableMeetingRooms);
     // return the available meeting rooms
     return availableMeetingRooms;
+  },
+  // write on change handlers for start time and end time
+  onStartTimeChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // update the formstate
+    setFormState({
+      ...formState,
+      startTime: {
+        value: event.target.value,
+        isValid: true,
+        errorMessage: "",
+      },
+    });
+  },
+  onEndTimeChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // update the formstate
+    setFormState({
+      ...formState,
+      endTime: {
+        value: event.target.value,
+        isValid: true,
+        errorMessage: "",
+      },
+    });
+  },
+  onSearchHandler = (event: any) => {
+    // prevent default behaviour
+    event.preventDefault();
+    // get the start time and end time from the form
+    const startTime = formState.startTime.value,
+    endTime = formState.endTime.value;
+    // find the available meeting rooms
+    const availableMeetingRooms = findAvailableMeetingRooms(startTime, endTime);
+    // update the state
+    setAvailableMeetingRooms(availableMeetingRooms);
   };
 
   return (
@@ -69,14 +129,14 @@ export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
             <div className="form-row">
               <div className="form-field">
                 <label>Start Time</label>
-                <input type="datetime-local" />
+                <input type="datetime-local" onChange={onStartTimeChangeHandler}/>
               </div>
               <div className="form-field">
                 <label>End Time</label>
-                <input type="datetime-local" />
+                <input type="datetime-local" onChange={onEndTimeChangeHandler}/>
               </div>
               <div className="form-field">
-                <button type="submit">Search</button>
+                <button type="button" onClick={onSearchHandler}>Search</button>
               </div>
             </div>
           </form>
@@ -84,13 +144,13 @@ export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
       </div>
       <div className="meeting-room-list">
         {/* generate a card component that displays details about each room */}
-        {meetingRooms.map((meetingRoom: any) => {
+        {availableMeetingRooms.map((meetingRoom: any) => {
           return (
             <div key={meetingRoom.id} className="meeting-room-card">
               <h2>{meetingRoom.name}</h2>
               <p>Capacity: {meetingRoom.capacity}</p>
               <p>Booked Time Slots:</p>
-              <ul>
+              {/* <ul>
                 {meetingRoom.bookedTimeSlots.map((bookedTimeSlot: any) => {
                   return (
                     <li>
@@ -98,7 +158,7 @@ export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
                     </li>
                   );
                 })}
-              </ul>
+              </ul> */}
             </div>
           );
         })}
