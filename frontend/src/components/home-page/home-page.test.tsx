953c2e863b175e83c8ad7ccc355a41e94869f4a9
harshanae99x <136785938+harshanae99x@users.noreply.github.com>
2023-08-17 16:24:10 +0530

    frontend init

---
+++ frontend/src/components/home-page/home-page.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { HomePage } from "./home-page";
+
+describe("HomePage", () => {
+  it("", () => {
+    render(<HomePage />);
+  });
+});
---
+++ frontend/src/components/home-page/home-page.tsx
@@ -0,0 +1,11 @@
+import "./home-page.scss";
+
+interface HomePageProps {}
+
+export const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
+  return (
+    <div className="home-page">
+      
+    </div>
+  );
+};
